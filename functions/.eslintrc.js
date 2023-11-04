317a4cb670b1e71973e793e8cecbb4e00bebed15
ree-rishun <ree0432@gmail.com>
2023-11-05 04:11:31 +0900

    feature: 検索サーバの作成

---
+++ functions/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    es6: false,
+    node: false,
+  },
+  parserOptions: {
+    'ecmaVersion': 2018,
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+  ],
+  rules: {
+    'no-restricted-globals': ['error', 'name', 'length'],
+    'prefer-arrow-callback': 'error',
+    'quotes': ['error', 'single', {'allowTemplateLiterals': true}],
+    'semi': ['error', 'never', {'beforeStatementContinuationChars': 'never'}],
+    'semi-spacing': ['error', {'after': true, 'before': false}],
+    'semi-style': ['error', 'first'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'max-len': ['error', {'code': 120}],
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.*'],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+}
---
+++ functions/index.js
@@ -0,0 +1,64 @@
+const {onCall} = require('firebase-functions/v2/https')
+const logger = require('firebase-functions/logger')
+const axiosBase = require('axios')
+const { Buffer } = require('node:buffer')
+
+const API_KEY = 'AIzaSyAeQ29pFqC9WcI-s8MLk8yUWZGInhu8sHQ'
+
+// 入力内容から候補スポットを取得
+exports.getCandidates = onCall(
+  async (req,) => {
+    logger.info(`req.data.text : ${req.data.text}`, {structuredData: true})
+
+    const baseUrl = 'https://places.googleapis.com/v1'
+
+    const axios = axiosBase.create({
+      baseURL: baseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': API_KEY,
+        'X-Goog-FieldMask': 'places.displayName,places',
+      },
+      responseType: 'json'
+    })
+
+    const res = await axios.post('/places:searchText', {
+      textQuery: req.data.text,
+      languageCode: 'ja',
+      locationBias: {
+        circle: {
+          center: {
+            latitude: 35.680959106959,
+            longitude: 139.76730676352,
+          },
+          radius: 50000,
+        },
+      },
+    })
+
+    logger.info(res.data, {structuredData: true})
+
+    return {
+      data: res.data,
+    }
+  })
+
+
+// 写真のURLを取得
+exports.getPhotoUrl = onCall(
+  async (req,) => {
+
+    const baseUrl = 'https://places.googleapis.com/v1'
+
+    const axios = axiosBase.create({
+      baseURL: baseUrl,
+    })
+
+    const res = await axios.get(`/${req.data.name}/media?key=${API_KEY}&maxHeightPx=400`)
+    logger.info(`https://places.googleapis.com/v1/${req.data.name}/media?key=${API_KEY}&maxHeightPx=100`, {structuredData: true})
+
+    return {
+      binary: res.data,
+    }
+  }
+)
